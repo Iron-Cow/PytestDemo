8c3d289289262b560f5269e2e958146b3dc35d93
Iron-Cow <uangeji@gmail.com>
2022-04-20 21:34:17 +0300

    tracing fixture execution demo

--- Pytest_topics/py_fixtures/conftest.py
+++ Pytest_topics/py_fixtures/conftest.py
@@ -1,5 +1,3 @@
-import os
-
 import pytest
 
 pytest.weekdays1 = ['mon', 'tue', 'wed']
--- Pytest_topics/py_fixtures/test_fixtures03.py
+++ Pytest_topics/py_fixtures/test_fixtures03.py
@@ -10,3 +10,19 @@ def test_remove_item(setup02):
     print(setup02)
     setup02.remove("thur")
     assert setup02 == pytest.weekdays2
+
+"""
+pytest -v -k fixtures03 --setup-show 
+
+
+collected 39 items / 37 deselected / 2 selected                                                                                                                                                                                                                         
+
+Pytest_topics/py_fixtures/test_fixtures03.py::test_del_item 
+    SETUP    M setup01
+        Pytest_topics/py_fixtures/test_fixtures03.py::test_del_item (fixtures used: setup01)PASSED
+Pytest_topics/py_fixtures/test_fixtures03.py::test_remove_item 
+        SETUP    F setup02
+        Pytest_topics/py_fixtures/test_fixtures03.py::test_remove_item (fixtures used: setup02)PASSED
+        TEARDOWN F setup02
+    TEARDOWN M setup01
+"""
