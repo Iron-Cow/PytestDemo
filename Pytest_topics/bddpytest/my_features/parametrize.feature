12b014db8b6553b95fa1db30eab5e3f9fd6667ec
Iron-Cow <uangeji@gmail.com>
2022-04-23 21:38:41 +0300

    bdd more parametrize

--- Pytest_topics/bddpytest/my_features/parametrize.feature
+++ Pytest_topics/bddpytest/my_features/parametrize.feature
@@ -6,3 +6,10 @@ Feature: Parametrizing tests in Pytest BDD
     Then There is same count of fruit kinds
     But If we add a different kind of fruit
     Then The count of kinds increases to 4
+
+    Scenario: Parametrize benefits
+      Given Given 5 fruits
+      When I eat 3
+      And I eat 2
+      Then I should have 0 fruits
+
--- Pytest_topics/bddpytest/test_bddparametrize.py
+++ Pytest_topics/bddpytest/test_bddparametrize.py
@@ -35,3 +35,20 @@ def add_different_fruit(fruits):
 @then(parsers.parse("The count of kinds increases to {count:d}"))
 def different_fruit_count(fruits, count):
     assert len(fruits) == count
+
+
+@given(parsers.parse("Given {count:d} fruits"), target_fixture='fruit_dict')
+def existing_fruits(count):
+    fruit_dict = {"have": count, "eat": 0, }
+    return fruit_dict
+
+
+@when(parsers.parse("I eat {eat:d}")) # used for I eat 3/I eat 2 at once
+def eat_3_fruits(fruit_dict: dict, eat: int):
+    fruit_dict["eat"] += eat
+    fruit_dict["have"] -= eat
+
+@then(parsers.parse("I should have {left:d} fruits"))
+def verify_resuts(fruit_dict: dict, count, left: int):
+    assert count - fruit_dict["eat"] == left
+    assert fruit_dict["have"] == left
