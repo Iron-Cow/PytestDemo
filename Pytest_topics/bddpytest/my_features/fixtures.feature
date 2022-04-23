68e766086af0e2233e33d50fe3baf754216eba34
Iron-Cow <uangeji@gmail.com>
2022-04-23 20:23:27 +0300

    pytest-bdd and fixtures

---
+++ Pytest_topics/bddpytest/my_features/fixtures.feature
@@ -0,0 +1,5 @@
+Feature: Fixtures and BDD background on Python Set DataType
+  Scenario: Adding to Set
+    Given A sent with 3 elements
+    When Add 2 elents to the set
+    Then The set should have 5 elements
---
+++ Pytest_topics/bddpytest/test_bddfixtures.py
@@ -0,0 +1,39 @@
+import pytest
+from pytest_bdd import scenario, scenarios, given, when, then
+from pathlib import Path
+
+feature_file_dir = "my_features"
+feature_file = "fixtures.feature"
+
+BASE_DIR = Path(__file__).resolve().parent
+FEATURE_FILE = BASE_DIR.joinpath(feature_file_dir).joinpath(feature_file)
+
+scenarios(FEATURE_FILE)
+
+@pytest.fixture()
+def setup_set() -> set:
+    print("\n In fixture... setup_set() function.. \n")
+    countries = {"Poland", "USA", "Israel", "Germany", "Ukraine"}
+    return countries
+
+
+@given("A sent with 3 elements", target_fixture="setup_set1")
+def set_of_elems(setup_set: set):
+    if len(setup_set) == 0:
+        pytest.xfail("The set of elements are empty")
+
+    while len(setup_set) > 3:
+        setup_set.pop()
+
+    return setup_set
+
+
+@when("Add 2 elents to the set")
+def add_elements(setup_set1: set):
+    setup_set1.add("India")
+    setup_set1.add("Australia")
+
+
+@then("The set should have 5 elements")
+def final_set_elements(setup_set1: set):
+    assert len(setup_set1) == 5
