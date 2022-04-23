d7533ec9baa5b7d5b646aabbd95bdb37de419597
Iron-Cow <uangeji@gmail.com>
2022-04-23 16:03:00 +0300

    pytest.ini configuring options demo

--- Pytest_topics/test_module02.py
+++ Pytest_topics/test_module02.py
@@ -1,4 +1,11 @@
-class TestMyTest:  # pytest -v Pytest_topics/test_module02.py::TestMyTest
+class TestMyTestSuite:  # pytest -v Pytest_topics/test_module02.py::TestMyTestSuite
+    def test_type(self):
+        assert type(1) == int
+
+    def test_strs(self):
+        assert str.upper("python") == "PYTHON"
+
+class TestMyTestWrongEnding:
     def test_type(self):
         assert type(1) == int
 
